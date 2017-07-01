512d18a3ca762c45b64ad902375a915eba2ee3aa
nikel96 <bachan_vokpp@mail.ru>
2017-07-02 00:05:44 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 if (isset($_GET['link'])) {
-	$shortlink = parse_ini_file('shortlink.ini');
-	if (array_key_exists($_GET['link'], $shortlink)) {
-		$link = str_replace('%3D', '=', $shortlink[$_GET["link"]]);
+	$shortlink = parse_ini_file("shortlink.ini");
+	if (array_key_exists($_GET["link"], $shortlink)) {
+		$link = str_replace(['%3D', '%25'], ['=', '&'], $shortlink[$_GET["link"]]);
 		header("Location: ".$link);
 		die;
 	} else {
